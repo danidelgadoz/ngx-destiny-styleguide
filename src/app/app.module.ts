3834db2e05d0062c705320df99e2b7054c9dfae5
Daniel Delgado <dedd1993@gmail.com>
2018-02-12 15:15:04 -0500

    styleguide module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StyleguideModule } from './styleguide/styleguide.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StyleguideModule
   ],
   providers: [],
   bootstrap: [AppComponent]
