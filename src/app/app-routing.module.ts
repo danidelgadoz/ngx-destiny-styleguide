2dace529186794782c03ab4c0ec077fccbc7c1d8
Daniel Delgado <dedd1993@gmail.com>
2018-02-15 17:19:31 -0500

    Routing implementation with introduction and sidenav-demo pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ComponentsDemoComponent } from './pages/components-demo/components-demo.component';
+import { SidenavDemoComponent } from './pages/components-demo/sidenav-demo/sidenav-demo.component';
+import { IntroduccionComponent } from './pages/introduccion/introduccion.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'componentes', pathMatch: 'full' },
+  {
+    path: 'introduccion',
+    component: IntroduccionComponent
+  },
+  {
+    path: 'componentes',
+    component: ComponentsDemoComponent,
+    children: [
+      { path: '', redirectTo: 'sidenav', pathMatch: 'full' },
+      { path: 'sidenav',  component: SidenavDemoComponent }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { StyleguideModule } from './styleguide/styleguide.module';
+import { ComponentsDemoComponent } from './pages/components-demo/components-demo.component';
+import { SidenavDemoComponent } from './pages/components-demo/sidenav-demo/sidenav-demo.component';
+import { IntroduccionComponent } from './pages/introduccion/introduccion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsDemoComponent,
+    SidenavDemoComponent,
+    IntroduccionComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     StyleguideModule
   ],
   providers: [],
