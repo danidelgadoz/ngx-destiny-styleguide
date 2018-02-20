ed86f6053f3cda9f7395f80911d5e68771292d0f
Daniel Delgado <dedd1993@gmail.com>
2018-02-20 11:47:39 -0500

    sidenav submenu toggle implementation done

--- src/app/styleguide/components/sidenav/sidenav.component.ts
+++ src/app/styleguide/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,12 +6,42 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  @ViewChild('sidenav') sidenavElement: ElementRef;
   @Input() toggle;
   @Input() fixed;
 
   constructor() { }
 
   ngOnInit() {
+    const submenuArray = this.sidenavElement.nativeElement.querySelectorAll('.submenu');
+
+    if (submenuArray.length > 0) {
+      submenuArray.forEach(submenu => {
+        submenu.setAttribute('default-height', submenu.querySelector('dd').clientHeight);
+        submenu.querySelector('dd').style.height = 0;
+
+        submenu.querySelector('dt').addEventListener('click', (e) => {
+          submenu.classList.toggle('open');
+          this.slideToggle(submenu.querySelector('dd'));
+
+          [].slice
+            .call(submenuArray)
+            .filter( _submenu => _submenu !== submenu )
+            .map((sibiling) => {
+              sibiling.querySelector('dd').style.height = 0;
+              sibiling.classList.remove('open');
+            });
+        });
+      });
+    }
+  }
+
+  slideToggle(element) {
+    if (element.style.height === '0px') {
+      element.style.height = element.parentElement.getAttribute('default-height') + 'px';
+    } else {
+      element.style.height = 0;
+    }
   }
 
 }
